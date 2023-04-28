c0aced3144b905b142bced3654afab4c84918158
Cyril Muchiri <cyrilmuchiri11@gmail.com>
2023-04-28 11:46:45 +0300

    Initial commit

---
+++ src/dates/DateTest.java
@@ -0,0 +1,10 @@
+package dates;
+
+import java.util.Date;
+
+public class DateTest {
+    public static void main(String [] args){
+        Date today=new Date();
+        System.out.println(today);
+    }
+}
